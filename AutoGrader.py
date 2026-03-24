7fb227118f668fa1ebeacf4d0aefd473de5514e5
pengyuhuang-sketch <pengyu.huang@gmail.com>
2026-03-24 16:52:01 +0800

    Update AutoGrader.py

--- AutoGrader.py
+++ AutoGrader.py
@@ -1,3 +1,10 @@
+import sys
+
+def get_resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+    
 import customtkinter as ctk
 from tkinter import filedialog, messagebox
 import pandas as pd
